cc5b69e2b51ea57051d9ae803e7c0fc2bad16c38
Tim Staudinger <t.staudinger96@web.de>
2022-08-16 21:17:35 +0200

    Created analyse_data.R

--- src/analyse_data.R
+++ src/analyse_data.R
@@ -6,12 +6,8 @@ data[1:5,]
 data[,"ocean_proximity"] <- as.factor(data[,"ocean_proximity"])
 summary(data)
 
-# Handle missing values in "total_bedrooms" (median)
-data$total_bedrooms[is.na(data$total_bedrooms)] <- median(data$total_bedrooms, na.rm=TRUE)
 
 
-# ------------------------------------------------------------------------------
-# Analyse original data set 
 # Graphical plots
 # Histograms
 par(mfrow = c(4, 3))
@@ -61,6 +57,8 @@ par(mfrow = c(1, 1))
 corr_matrix <- round(cor(data[,1:9], method="spearman"), digits = 2)
 corrplot(corr_matrix, method= "number", tl.cex = 1, type = "upper")
 
+
+
 # Statistical indicators
 # Calculate MAD since we have many outliers
 mad(data[,"longitude"], constant = 1)
@@ -72,35 +70,3 @@ mad(data[,"population"], constant = 1)
 mad(data[,"households"], constant = 1)
 mad(data[,"median_income"], constant = 1)
 mad(data[,"median_house_value"], constant = 1)
-
-
-# ------------------------------------------------------------------------------
-# Cleaning data
-# Create helper variables (easier to decide if value is possible)
-data$people_per_household = data$population/data$households
-data$rooms_per_person = data$total_rooms/data$population
-
-# Add variable which contains two other and drop the others (Multikollinearität)
-data$beds_per_room = data$total_bedrooms/data$total_rooms
-
-# Remove improbable records
-data<-data[!(data$median_house_value>=500001),]
-data<-data[!(data$housing_median_age>=52),]
-data<-data[!(data$median_income>=15.0001),]
-data<-data[!(data$beds_per_room>1),]
-
-# Export data -> easier to handle outliers
-library("xlsx")
-write.xlsx2(data, file = "/Users/timstaudinger/Desktop/Repository.nosync/MachineLearning-Exam/dataset/housing_new.xlsx",
-            sheetName = "Sheet1", append = FALSE, row.names = TRUE)
-
-# Import cleaned data set and check if everything correct
-setwd("/Users/timstaudinger/Desktop/Repository.nosync/MachineLearning-Exam/dataset")
-data <- read.csv2("housing_cleaned.csv",header=TRUE,sep=";",fill=TRUE,stringsAsFactors=TRUE)
-data[1:5,]
-summary(data)
-
-# Remove (helper-) variables
-drops = c('total_bedrooms', 'total_rooms', 'people_per_household', 'rooms_per_person', 'X')
-data = data[ , !(names(data) %in% drops)]
-summary(data)
